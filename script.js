c3ad6765ebd4d2d59f17d03cd0237553312e071d
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-28 16:37:32 +0530

    script.js

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ buttonPress.addEventListener('click',()=>{
     getWeatherReport(searchInputBox.value);
 });
 
+
 function getWeatherReport(city) {
     fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)  
         .then(weather => {   
